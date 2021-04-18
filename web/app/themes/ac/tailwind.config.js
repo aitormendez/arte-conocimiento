bdb89b27e152f5dcc29e690d9e90937ddd54b6b9
aitor <aitor@e451.net>
2021-04-18 17:30:06 +0200

    Fuente Rubik

--- web/app/themes/ac/tailwind.config.js
+++ web/app/themes/ac/tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['rubik', 'sans-serif'],
+        'serif': ['times-new-roman', 'serif'],
+       },
       colors: {},
     },
   },
