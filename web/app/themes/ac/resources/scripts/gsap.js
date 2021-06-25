061d2743a51f707e0f6ae0f05b90cb1fd9153f74
aitor <aitor@e451.net>
2021-06-25 08:29:47 +0200

    Gradientes random funcionando

--- web/app/themes/ac/resources/scripts/gsap.js
+++ web/app/themes/ac/resources/scripts/gsap.js
@@ -2,27 +2,14 @@ import {
   TweenMax,
   TimelineLite,
   Power2,
-  Elastic,
-  CSSPlugin
-} from "gsap/all";
+  Elastic
+} from 'gsap/all';
 
-let R, G, B, dur;
 
-const setVars = () => {
-  R = Math.floor((Math.random() * 255)),
-    G = Math.floor((Math.random() * 255)),
-    B = Math.floor((Math.random() * 255)),
-    dur = Math.floor((Math.random() * 4000));
-}
-
-const getColor = () => {
-  setVars();
-  return `rgb(${R}, ${G}, ${B})`;
-};
-
-setVars();
-
-TweenMax.to(".colores", {
-  duration: 1,
-  backgroundColor: 'white',
+TweenMax.to('.colores', {
+  duration: 'random(2, 6)',
+  background: 'radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 0%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 100%),radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 0%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 100%),radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 0%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 100%)',
+  repeat: -1,
+  repeatRefresh: true,
+  ease: "sine.inOut"
 });
