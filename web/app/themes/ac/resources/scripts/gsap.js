71074fb938367a939283bada6f754a5436f09ca7
aitor <aitor@e451.net>
2021-06-25 10:30:46 +0200

    Gradientes ok comentados

--- web/app/themes/ac/resources/scripts/gsap.js
+++ web/app/themes/ac/resources/scripts/gsap.js
@@ -7,8 +7,8 @@ import {
 
 
 TweenMax.to('.colores', {
-  duration: 'random(2, 6)',
-  background: 'radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 0%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 100%),radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 0%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 100%),radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 0%, rgba(random(0, 255, 1), random(0, 255, 1), random(0, 255, 1), random(0, 0.5)) 100%)',
+  duration: 'random(10, 20)',
+  background: 'radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.76) 0%, rgba(255, 255, 255, 0) 100%),radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(255, 245, 0, 0.76) 0%, rgba(255, 255, 255, 0) 100%),radial-gradient(random(0, 200)% random(0, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.76) 0%, rgba(255, 255, 255, 0) 100%)',
   repeat: -1,
   repeatRefresh: true,
   ease: "sine.inOut"
