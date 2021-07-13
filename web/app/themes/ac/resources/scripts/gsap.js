4057f227714ba825ecf53867a75be965f01e9aa1
aitor <aitor@e451.net>
2021-07-13 11:32:16 +0200

    Animación radial gradient funcionando OK en todos los navegadores

--- web/app/themes/ac/resources/scripts/gsap.js
+++ web/app/themes/ac/resources/scripts/gsap.js
@@ -1,17 +1,18 @@
 import {
-  TweenMax,
-  TimelineLite,
-  Power2,
-  Elastic
+ gsap
 } from 'gsap/all';
 
+gsap.set('.colores', {
+  backgroundImage: 'radial-gradient(100% 102.5% at 16% 80%, rgba(0, 133, 255, 0.76) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(100% 102% at 80% 20%, rgba(0, 133, 255, 0.76) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(100% 100% at 20% 80%, rgba(255, 245, 0, 0.76) 0%, rgba(255, 255, 255, 0) 100%)'
+})
 
-// TweenMax.to('.colores', {
-//   duration: 'random(10, 20)',
-//   background: 'radial-gradient(random(100, 200)% random(100, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.76) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(random(100, 200)% random(100, 200)% at random(0, 100)% random(0, 100)%, rgba(255, 245, 0, 0.76) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(random(100, 200)% random(100, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.76) 0%, rgba(255, 255, 255, 0) 100%)',
-//   repeat: -1,
-//   repeatRefresh: true,
-//   ease: "sine.inOut"
-// });
+
+gsap.to('.colores', {
+  duration: '2',
+  backgroundImage: 'radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.76) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(255, 245, 0, 0.76) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.76) 0%, rgba(255, 255, 255, 0) 100%)',
+  repeat: -1,
+  repeatRefresh: true,
+  ease: "sine.inOut"
+});
 
 
--- web/app/themes/ac/resources/views/partials/header.blade.php
+++ web/app/themes/ac/resources/views/partials/header.blade.php
@@ -1,9 +1,11 @@
 <header class="banner">
   <a class="brand" href="{{ home_url('/') }}">
-    {{ $siteName }}
+    <span class="block">Prácticas Artísticas</span>
+    <span class="block">y Formas de Conocimiento</span>
+    <span class="block">Contemporáneas</span>
   </a>
 
-  <nav class="solapa">
+  <nav class="solapa absolute">
     @include('partials.navigation')
     <button id="hamb">
       @svg('images.hamb', 'fixed top-5 right-5')
