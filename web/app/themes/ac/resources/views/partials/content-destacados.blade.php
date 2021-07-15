3498a70dd655db8d957cb96b865a025e7adde028
aitor <aitor@e451.net>
2021-07-15 10:59:42 +0200

    Mosaico destacados front page (2)

--- web/app/themes/ac/resources/views/partials/content-destacados.blade.php
+++ web/app/themes/ac/resources/views/partials/content-destacados.blade.php
@@ -1,27 +1,27 @@
-<article class="p-4 mb-6 lg:mb-0 {{ $destacado['post_type'] }} {{ $destacado['tamano'] }} {{ $destacado['formato'] }} relative">
+<article class="p-4 mb-6 md:mb-0 {{ $destacado['post_type'] }} {{ $destacado['tamano'] }} {{ $destacado['formato'] }} relative">
 
   {{-- IMAGEN --}}
 
-  <a href="{{ $destacado['link'] }}" class="flex flex-wrap w-full">
-    <header class="w-full col-datos md:flex md:flex-col md:justify-between">
-      <div class="arriba">
-        <div class="text-sm font-bold tracking-widest uppercase">{{ $destacado['post_type'] }}</div>
-        <h2 class="my-6 text-2xl tracking-wider">{{ $destacado['title'] }}</h2>
-      </div>
-      <div class="entry-meta">
-        @include('partials/entry-meta')
-      </div>
-    </header>
+  <a href="{{ $destacado['link'] }}" class="flex flex-wrap text-black hover:text-black interior">
+    <div class="arriba md:flex-1">
+      <header class="w-full p-4">
+          <div class="text-sm font-bold tracking-widest uppercase tipo">{{ $destacado['post_type'] }}</div>
+          <h2 class="my-6 text-2xl tracking-wider">{{ $destacado['title'] }}</h2>
+        <div class="entry-meta">
+          @include('partials/entry-meta')
+        </div>
+      </header>
+    </div>
     @if ($destacado['contenido'] === 'imagen')
       @if ($destacado['has_img'])
-        <div class="abajo img">
-          <img src="{!! $destacado['url'] !!}" srcset="{!! $destacado['srcset'] !!}" alt="{!! $destacado['alt'] !!}" sizes="(max-width: 768px) 100vw, 40vw">
+        <div class="flex-none abajo img">
+          <img class="w.full" src="{!! $destacado['url'] !!}" srcset="{!! $destacado['srcset'] !!}" alt="{!! $destacado['alt'] !!}" sizes="(max-width: 768px) 100vw, 40vw">
         </div>
       @endif
     @endif
     @if ($destacado['contenido'] === 'texto')
       @if ($destacado['has_txt'])
-        <div class="abajo txt">
+        <div class="p-4 abajo txt">
           {!! $destacado['txt'] !!}
         </div>
       @endif
--- web/app/themes/ac/resources/views/partials/header.blade.php
+++ web/app/themes/ac/resources/views/partials/header.blade.php
@@ -1,15 +1,15 @@
 <header class="p-4 banner">
-  <a class="brand" href="{{ home_url('/') }}">
+  <a class="inline-block brand" href="{{ home_url('/') }}">
     <span class="block">Prácticas Artísticas</span>
     <span class="block">y Formas de Conocimiento</span>
     <span class="block">Contemporáneas</span>
   </a>
 
-  <nav class="fixed top-0 p-4 bg-white solapa">
+  <nav class="fixed top-0 z-40 p-4 bg-white solapa">
     @include('partials.navigation')
   </nav>
 
-  <button id="hamb" class="fixed flex flex-col justify-between top-4 right-4 lg:hidden">
+  <button id="hamb" class="fixed z-50 flex flex-col justify-between top-4 right-4 lg:hidden">
     <b class="block w-full bg-black h"></b>
     <b class="block w-full bg-black h"></b>
     <b class="block w-full bg-black h"></b>
