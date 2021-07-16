279020efd5e34d81495d3221f89a35473b841ece
aitor <aitor@e451.net>
2021-07-16 16:35:15 +0200

    Revita Accesos. Enlace en front page (3)

--- web/app/themes/ac/resources/scripts/gsap.js
+++ web/app/themes/ac/resources/scripts/gsap.js
@@ -25,11 +25,11 @@ document.querySelectorAll('.colores').forEach((item, i) => {
 
 
 document.querySelectorAll('.color-ya').forEach((item, i) => {
-        item.id = 'color-' + i;
-        gsap.set('#color-' + i, {
+        item.id = 'colorya-' + i;
+        gsap.set('#colorya-' + i, {
             backgroundImage: 'radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(255, 245, 0, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%)',
         })
-        gsap.to('#color-' + i, {
+        gsap.to('#colorya-' + i, {
             duration: '1',
             backgroundImage: 'radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(255, 245, 0, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%)',
             repeat: -1,
