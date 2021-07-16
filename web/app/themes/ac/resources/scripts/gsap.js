5a7b313b0befd02625f9580b5b9d8965a0c0f119
aitor <aitor@e451.net>
2021-07-16 16:18:33 +0200

    Revita Accesos. Enlace en front page

--- web/app/themes/ac/resources/scripts/gsap.js
+++ web/app/themes/ac/resources/scripts/gsap.js
@@ -22,3 +22,20 @@ document.querySelectorAll('.colores').forEach((item, i) => {
         gsap.killTweensOf('#color-' + i);
     })
   })
+
+
+document.querySelectorAll('.color-ya').forEach((item, i) => {
+        item.id = 'color-' + i;
+        gsap.set('#color-' + i, {
+            backgroundImage: 'radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(255, 245, 0, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%)',
+        })
+        gsap.to('#color-' + i, {
+            duration: '1',
+            backgroundImage: 'radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(255, 245, 0, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%), radial-gradient(random(100, 200)% random(50, 200)% at random(0, 100)% random(0, 100)%, rgba(0, 133, 255, 0.9) 0%, rgba(255, 255, 255, 0.3) 100%)',
+            repeat: -1,
+            repeatRefresh: true,
+            ease: "sine.inOut",
+        });
+  })
+
+  
--- web/app/themes/ac/resources/views/front-page.blade.php
+++ web/app/themes/ac/resources/views/front-page.blade.php
@@ -6,6 +6,13 @@
 
 
     <section id="destacados" class="">
+      <article class="cuadrado pequeño">
+        <div class="h-full p-4 interior">
+          <div class="flex items-center h-full p-4 font-bold tracking-widest text-center uppercase border-8 border-black color-ya">
+            <span class="block">Revista Accesos</span>
+          </div>
+        </div>
+      </article>
       @foreach ($destacados['posts'] as $destacado)
         @include('partials.content-destacados')
       @endforeach
