7bc51bc24c1febdb84f4ab9515c452144b562158
aitor <aitor@e451.net>
2021-07-18 10:31:40 +0200

    Ajustes menú mobile

--- web/app/themes/ac/resources/views/partials/navigation-desktop.blade.php
+++ web/app/themes/ac/resources/views/partials/navigation-desktop.blade.php
@@ -1,5 +1,5 @@
 @if ($navigation_desktop)
-  <ul class="items-start h-0 leading-tight desktop md:flex">
+  <ul class="items-start hidden h-0 leading-tight desktop md:flex">
     @foreach ($navigation_desktop as $item)
       <li class="p-4 my-menu-item-desktop relative {{ $item->classes ?? '' }} {{ $item->active ? 'active' : '' }} font-bold">
         @if ($item->children)
