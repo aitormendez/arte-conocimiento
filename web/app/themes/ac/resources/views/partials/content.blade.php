73bc7127f4907855013506588564b94d85d60a16
aitor <aitor@e451.net>
2021-08-08 17:21:25 +0200

    Archivo. CSS inicial

--- web/app/themes/ac/resources/views/partials/content.blade.php
+++ web/app/themes/ac/resources/views/partials/content.blade.php
@@ -1,15 +1,17 @@
-<article @php(post_class())>
-  <header>
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
-        {!! $title !!}
-      </a>
-    </h2>
-
-    @include('partials/entry-meta')
-  </header>
-
-  <div class="entry-summary">
-    @php(the_excerpt())
+<article @php(post_class('mb-4 p-4 bg-white'))>
+  <div class="wrap lg:mx-auto lg:max-w-3xl">
+    <header>
+      <h2 class="mb-3 text-xl font-bold entry-title">
+        <a href="{{ get_permalink() }}">
+          {!! $title !!}
+        </a>
+      </h2>
+      <div class="text-xs meta">
+        @include('partials/entry-meta')
+      </div>
+    </header>
+    <div class="entry-summary">
+      @php(the_excerpt())
+    </div>
   </div>
 </article>
--- web/app/themes/ac/resources/views/partials/page-header.blade.php
+++ web/app/themes/ac/resources/views/partials/page-header.blade.php
@@ -1,3 +1,3 @@
-<div class="prose page-header lg:mx-auto lg:max-w-3xl">
+<div class="pb-20 prose page-header lg:mx-auto lg:max-w-3xl">
   <h1>{!! $title !!}</h1>
 </div>
