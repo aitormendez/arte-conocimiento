6d8609c8efcd94e90701d11706a9f62e34994fc7
aitor <aitor@e451.net>
2021-08-09 18:41:34 +0200

    Afinar el infinite scroll

--- web/app/themes/ac/resources/scripts/infscroll.js
+++ web/app/themes/ac/resources/scripts/infscroll.js
@@ -4,6 +4,7 @@ $(document).ready(() => {
   if (document.body.classList.contains('archive')) {
 
     let viewMoreButton = document.querySelector('.view-more-button');
+    let ulti = false;
 
     let infScroll = new InfiniteScroll( '.infinite-scroll-container', {
       path: '.nav-previous a',
@@ -16,8 +17,12 @@ $(document).ready(() => {
 
     infScroll.on( 'load', onPageLoad );
 
+    infScroll.on( 'last', function() {
+      ulti = true;
+    } );
+
     function onPageLoad() {
-    if ( infScroll.loadCount == 1 ) {
+    if ( infScroll.loadCount == 1 && !ulti ) {
         // after 2nd page loaded
         // disable loading on scroll
         infScroll.options.loadOnScroll = false;
