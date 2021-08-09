65dbf2013a94592ad02434a4afe2248d7e4ffdf9
aitor <aitor@e451.net>
2021-08-09 11:07:45 +0200

    Archivo. Mejorar animación thumbnail

--- web/app/themes/ac/resources/scripts/archive.js
+++ web/app/themes/ac/resources/scripts/archive.js
@@ -11,34 +11,75 @@ $(document).ready(() => {
     
     document.querySelectorAll('.thumb').forEach((item, i) => {
         item.id = 'giro-' + i;
+        let cambiando = false;
+
+        function start() {
+            cambiando = true;
+            console.log(cambiando);
+        }
+
+        function complete() {
+            cambiando = false;
+            console.log(cambiando);
+        }
 
         gsap.set('#giro-' + i, {
             rotation: 'random(-10, 10)',
         })
 
-        item.addEventListener('mouseenter', event => {
+        function abrirRotar(){
             gsap.to('#giro-' + i, {
                 duration: '0.5',
                 rotation: 'random(340, 380)',
                 ease: "power1.inOut",
+                overwrite: 'auto',
             });
+        } 
+
+        function abrirCrecer() {
             gsap.to('#giro-' + i, {
                 duration: '0.5',
                 maxWidth: '50vw',
                 ease: "power1.inOut",
+                overwrite: 'auto',
+                onStart: () =>  start(),
+                onComplete: () =>  complete(),
             });
-        })
-        item.addEventListener('mouseleave', event => {
+        }
+
+        function cerrarRotar() {
             gsap.to('#giro-' + i, {
                 duration: '0.5',
                 rotation: 'random(-20, 20)',
                 ease: "power1.inOut",
+                overwrite: 'auto',
             });
+        } 
+
+        function cerrarCrecer() {
             gsap.to('#giro-' + i, {
                 duration: '0.5',
                 maxWidth: '10vw',
                 ease: "power1.inOut",
+                onStart: () =>  start(),
+                onComplete: () =>  complete(),
+                overwrite: 'auto',
             });
+        } 
+
+        item.addEventListener('mouseenter', event => {
+
+            if (!cambiando) {
+                abrirRotar();
+                abrirCrecer();
+            }
+
+        })
+        item.addEventListener('mouseleave', event => {
+            if (!cambiando) {
+                cerrarRotar();
+                cerrarCrecer();
+            }
         })
         
     });
