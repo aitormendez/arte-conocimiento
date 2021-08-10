5645560ca97596adf8b452388fe5a3f8df34ed98
aitor <aitor@e451.net>
2021-08-10 10:56:29 +0200

    Archive. Convertir cada article entero en enlace

--- web/app/themes/ac/resources/views/partials/content.blade.php
+++ web/app/themes/ac/resources/views/partials/content.blade.php
@@ -1,4 +1,4 @@
-<article @php(post_class('mb-4 p-4 bg-white infinite-scroll-item'))>
+<a role="article" href="{{ get_permalink() }}" class="block p-4 mb-4 bg-white normal infinite-scroll-item">
   <div class="wrap lg:mx-auto lg:max-w-3xl">
     @if (has_post_thumbnail())
       <div class="pb-4 thumb">
@@ -8,16 +8,14 @@
 
     <header>
       <h2 class="mb-3 text-xl font-bold entry-title">
-        <a href="{{ get_permalink() }}">
-          {!! $title !!}
-        </a>
+        {!! $title !!}
       </h2>
       <div class="text-xs text-gray-400 meta">
         @include('partials/entry-meta')
       </div>
     </header>
-    <div class="entry-summary">
+    <div class="text-black entry-summary">
       @php(the_excerpt())
     </div>
   </div>
-</article>
+</a>
